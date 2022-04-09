ca16e81c6e2ea8e8b42b4309407f076adb1450fe
Tani1964 <ifegbesan6@gmail.com>
2022-04-08 17:28:38 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+char c;
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return(0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c = '9'; c++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
