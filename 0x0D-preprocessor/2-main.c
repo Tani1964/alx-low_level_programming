a84f8445ff52cfe4f2ac03044602f6ca136e2669
Tani1964 <ifegbesan6@gmail.com>
2022-05-12 10:21:35 -0700

    3-function_like_macro.h2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
+/**
+ * main - Write a program that prints the name of the file it was compiled
+ * from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
+
 	printf("%s\n", __FILE__);
+	return (0);
 }
